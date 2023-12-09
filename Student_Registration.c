95d4d87546c40ce7b21e777860f5c72d77a7aaf5
Srikar-jayanthi <147791947+Srikar-jayanthi@users.noreply.github.com>
2023-12-09 11:47:21 +0530

    #include<stdio.h> int main() {     int n,a,b,c,d,e,f;     scanf("%d%d%d%d%d%d%d",&n,&a,&b,&c,&d,&e,&f);     if(b-c>=a)     {         printf("YES ");
        }
        else
        {
            printf("NO
    ");
        }
        if(e-f>=d)
        printf("YES");
        else
        printf("NO");
        
    }

---
+++ Student_Registration.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,b,c,d,e,f;
+    scanf("%d%d%d%d%d%d%d",&n,&a,&b,&c,&d,&e,&f);
+    if(b-c>=a)
+    {
+        printf("YES
+");
+        
+    }
+    else
+    {
+        printf("NO
+");
+    }
+    if(e-f>=d)
+    printf("YES");
+    else
+    printf("NO");
+    
+}

ddfdb1e5b20ef144da2af350a3498f2845237056
Jayanthi Srikar <147791947+Srikar-jayanthi@users.noreply.github.com>
2024-01-20 21:17:07 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int sum=0;     for(i=0;i<n;i++)     {         sum=sum+arr[i];     }     int c=0;     for(i=0;i<n;i++)     {         if(arr[i]>=(sum/n))         {             c++;         }     }     printf("%d",c);
    }

---
+++ Elements_greater_than_average.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int sum=0;
+    for(i=0;i<n;i++)
+    {
+        sum=sum+arr[i];
+    }
+    int c=0;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>=(sum/n))
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+    
+    
+    
+}
